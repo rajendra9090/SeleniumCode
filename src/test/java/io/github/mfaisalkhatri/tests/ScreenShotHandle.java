e39472e091d42ba8b92a2a9e30bc094601197b28
RajendraKumarBelandu <RajendraKumarBelandu@CILPLAP00068>
2024-02-25 12:05:36 +0530

    ser

--- src/test/java/io/github/mfaisalkhatri/tests/ScreenShotHandle.java
+++ src/test/java/io/github/mfaisalkhatri/tests/ScreenShotHandle.java
@@ -1,5 +1,13 @@
 package io.github.mfaisalkhatri.tests;
 
+import org.openqa.selenium.WebDriver;
+
 public class ScreenShotHandle {
+static WebDriver driver;
+public static void main(String[] arg)
+System.setProperty("webdriver.chrome.driver, null)
+
+
+
 
 }
